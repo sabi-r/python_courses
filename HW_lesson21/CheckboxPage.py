9aea0cb254103bff3b5cd0a072f3f1476ebadf87
Sabina <sabina@MacBook-Air-Sabina-2.local>
2024-03-19 17:41:43 +0200

    added HW_lesson_21

--- HW_lesson21/CheckboxPage.py
+++ HW_lesson21/CheckboxPage.py
@@ -4,6 +4,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 URL = "https://demoqa.com/checkbox"
 
+
 class CheckboxPage:
     def __init__(self, driver):
         self.driver: WebDriver = driver
@@ -13,7 +14,8 @@ class CheckboxPage:
         return self
 
     def expand_folder(self, name) -> None:
-        versatile_checkbox_button = self.driver.find_element(By.XPATH, f"//label[contains(@for, 'tree-node-{name}')]//ancestor::span/button")
+        versatile_checkbox_button = self.driver.find_element(By.XPATH,
+                                                             f"//label[contains(@for, 'tree-node-{name}')]//ancestor::span/button")
         try:
             expand = versatile_checkbox_button.find_element(By.XPATH, "//*[contains(@class, 'expand-open')]")
             if expand.is_displayed():
@@ -33,24 +35,32 @@ class CheckboxPage:
             pass
         versatile_checkbox_button.click()
 
-
     def mark_folder(self, name):
         versatile_checkbox_button = self.driver.find_element(By.XPATH, f"//label[contains(@for, 'tree-node-{name}')]")
         input_field = self.driver.find_element(By.XPATH, f"//label[contains(@for, 'tree-node-{name}')]/input")
         if not input_field.is_selected():
             versatile_checkbox_button.click()
 
-
     def unmark_folder(self, name):
-        versatile_checkbox_button = self.driver.find_element(By.XPATH, f"//label[contains(@for, 'tree-node-{name}')]")
-        # input_field = self.driver.find_element(By.XPATH, f"//label[contains(@for, 'tree-node-{name}')]/input")
+        versatile_checkbox_button = self.driver.find_element(By.XPATH,
+                                                             f"//label[contains(@for, 'tree-node-{name}')]/span[contains(@class, 'checkbox')]")
+        try:
+            input_field = self.driver.find_element(By.XPATH, "//*[contains(@class, 'uncheck')]")
+            if not input_field.is_displayed():
+                input_field.click()
+        except NoSuchElementException:
+            pass
         versatile_checkbox_button.click()
 
+    def get_text_results(self):
+        list_message = []
+        messages = [message.text for message in
+                    self.driver.find_elements(By.XPATH, "//span[@class='text-success']")]
+        list_message.extend(messages)
+        return list_message
 
+    def scroll_down(self) -> None:
+        self.driver.execute_script("window.scrollBy(0, 200);")
 
-
-
-
-
-        # //*[contains(@class, "expand-open")] - відкрито
-        # //*[@class="rct-icon rct-icon-expand-close"]
+    # //*[contains(@class, "expand-open")] - відкрито
+    # //*[@class="rct-icon rct-icon-expand-close"]
--- HW_lesson21/tests/test_checkbox.py
+++ HW_lesson21/tests/test_checkbox.py
@@ -1,9 +1,10 @@
 import pytest
 from HW_lesson21.CheckboxPage import CheckboxPage
 
-@pytest.mark.usefixtures("firefox")
+
+@pytest.mark.usefixtures("chrome_class")
 class TestCheckboxPage:
-    def setup_method(self):
+    def setup(self):
         self.page = CheckboxPage(self.driver)
 
     def test_page(self):
@@ -13,3 +14,14 @@ class TestCheckboxPage:
         self.page.collapse_folder("home")
         self.page.unmark_folder("home")
         pass
+
+    def test_checkboxes(self):
+        self.page.open()
+        self.page.scroll_down()
+        self.page.expand_folder("home")
+        self.page.mark_folder("desktop")
+        self.page.expand_folder("documents")
+        self.page.mark_folder("office")
+        actual_text = self.page.get_text_results()
+        expected_text = ["desktop", "notes", "commands", "office", "public", "private", "classified", "general"]
+        assert actual_text == expected_text
